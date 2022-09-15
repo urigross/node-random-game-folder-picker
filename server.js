09fb87c11a5eb22b886b6167846faccf0d43c3f8
urigross <urigross@gmail.com>
2022-09-15 15:47:01 +0300

    Added prompts for Self execution files. Added prompt for user choice of path

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-// Taget: Pick random game folder from games directory. Some of the games are just with empty directories so they are not valid
-// So the program filters them out before random picking
+// Tagret: Pick random folder from user prompt directory. Some of the sub-folders are just with empty directories so they are not valid
+// So the program filters them out before random picking 1 folder.
 
-const express = require('express');
-const app = express();
 const fs = require('fs');
-
+var prompt = require('prompt-sync')();
 function start() {
     // const path = 'f:/games/';
-    const pathName = 'c:/downloadz/';
-    if (isValidPath(pathName)) pickRndFolderWithFiles(pathName);
-
+    
+    var pathName = prompt('Type path To scan for folders with files and randomise one of them: ');
+    pathName = pathName+'/';
+    isValidPath(pathName).then(() => {
+        pickRndFolderWithFiles(pathName);
+    }
+    );
 };
 
 function isValidPath(pathName) {
-    fs.stat(pathName, (error, stats) => {
-        // incase of error
-        if (error) {
-            console.error('not Valid Path');
-            return false;
-        }        
+    return new Promise(function (resolve, reject) {
+        fs.stat(pathName, (error, stats) => {
+            // in case of error
+            if (error) {
+                displayPauseMsg(`Error: ${pathName} is not a valid Path.\n Press any key to continue.`);
+                return reject(error)
+            }
+            return resolve(stats)
+        })
     })
-    return true;
 };
-
-start();
-
 function pickRndFolderWithFiles(pathName) {
     fs.readdir(pathName, { withFileTypes: true }, (err, folders) => {
         const filteredFolders = folders
@@ -39,13 +40,15 @@ function pickRndFolderWithFiles(pathName) {
             // Check if folder empty. if not add folder
             if (returnedFiles.length !== 0) gameFolderNames.push(folder);
         });
-        console.log('Chosen random folder that containing files is: ',getRandFile(gameFolderNames));
+        ;
+        displayPauseMsg(`\n\n\nChosen Folder: ${getRandFile(gameFolderNames)}.\n\nPress any key to end.`);
     });
 }
-
-
+function displayPauseMsg(msg) {
+    var input = prompt(msg);
+}
 function getRandFile(gameFolderNames) {
     var rndIdx = Math.floor(Math.random() * gameFolderNames.length);
     return gameFolderNames[rndIdx];
 };
-
+ start();
